f55e3a3f64687479d1ad1e6ab0c8c855eaf2ab10
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-13 22:17:46 +0300

    Executing multiple coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Coroutine that uses an imported async_comprehension."""
-
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -8,5 +8,5 @@ async def wait_n(n: int, wait_delay: int) -> list[float]:
     """Coroutine that takes in 2 int arguments (in this order): n and
     max_delay and returns the list of all the delays (float values)
     from wait_random coroutine"""
-    delays = [await wait_random(wait_delay) for _ in range(n)]
+    delays: List[float] = [await wait_random(wait_delay) for _ in range(n)]
     return sorted(delays)

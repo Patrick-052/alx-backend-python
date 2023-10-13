8a817c4d3e0fd4ee6b448f0836ed2277966f5517
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-13 22:24:00 +0300

    Fixing syntax bugs and annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,5 +8,6 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     """Coroutine that takes in 2 int arguments (in this order): n and
     max_delay and returns the list of all the delays (float values)
     from wait_random coroutine"""
-    delays: List[float] = [await wait_random(max_delay) for _ in range(n)]
+    delays: List[float] = []
+    delays = [await wait_random(max_delay) for _ in range(n)]
     return sorted(delays)

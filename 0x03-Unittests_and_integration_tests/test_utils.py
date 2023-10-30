ccfa516cf967011782bbc720af515f130f798aa0
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-30 22:30:31 +0300

    Paremeterizing unit tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,10 +2,9 @@
 """ Module that contains the test suite for utils.py """
 
 from unittest import TestCase
-import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(TestCase):
@@ -44,3 +43,29 @@ class TestGetJson(TestCase):
         mocked_get.return_value.json.return_value = test_payload
         self.assertEqual(get_json(test_url), test_payload)
         mocked_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(TestCase):
+    """ Test class for the utils.memoize decorator """
+
+    def test_memoize(self):
+        """ Test case to acertain that the memoize decorator
+            memoizes a function's output correctly """
+        class TestClass:
+            """ Test class to test the memoize decorator """
+
+            def a_method(self):
+                """ Method that returns a number """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ Method that returns a number """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mocked_method:
+            mocked_method.return_value = 42
+            instance = TestClass()
+            self.assertEqual(instance.a_property, mocked_method.return_value)
+            self.assertEqual(instance.a_property, mocked_method.return_value)
+            mocked_method.assert_called_once()

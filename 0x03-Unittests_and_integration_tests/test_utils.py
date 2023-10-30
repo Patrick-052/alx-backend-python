6a82aa5fca9673a49186e0498b347c31aebf771a
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-30 20:38:08 +0300

    Parameterizing a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,5 +15,5 @@ class TestAccessNestedMap(TestCase):
         ({'a': {'b': 2}}, ('a', 'b'), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """ Test the access_nested_map function """
+        """ Test case for the access_nested_map function """
         self.assertEqual(access_nested_map(nested_map, path), expected)

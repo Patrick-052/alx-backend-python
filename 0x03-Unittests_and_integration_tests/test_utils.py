50c95112966651c02f95ac836e78cbab8d15e069
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-30 21:49:14 +0300

    Paremeterizing unit tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,12 +2,14 @@
 """ Module that contains the test suite for utils.py """
 
 from unittest import TestCase
+import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(TestCase):
-    """ Test class for the utils functions """
+    """ Test class for the utils.access_nested_map function """
 
     @parameterized.expand([
         ({'a': 1}, ('a',), 1),
@@ -26,3 +28,19 @@ class TestAccessNestedMap(TestCase):
         """ Test case for the access_nested_map function for an exception """
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """ Test class for the utils.get_json function """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mocked_get):
+        """ Test case to acertain that the get_json function
+            returns the expected result """
+        mocked_get.return_value.json.return_value = test_payload
+        self.assertEqual(get_json(test_url), test_payload)
+        mocked_get.assert_called_once_with(test_url)
